cbb2d1ad7eb4f99453606859a6792acbb8111c7a
aurora324 <3050517330@qq.com>
2023-12-27 18:07:21 +0800

    更改

--- src/p101.java
+++ src/p101.java
@@ -1,19 +1,104 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class p101 {
     public static void main(String[] args) {
-        QReader in=new QReader();
-        QWriter out=new QWriter();
-        int a=in.nextInt();
+        QReader in = new QReader();
+        QWriter out = new QWriter();
 
         //a b d f l k
 
         //abc
         //
         //
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        node[] nodes = new node[n + 1];
+        for (int i = 1; i < nodes.length; i++) {
+            nodes[i] = new node(i, in.nextLong());
+        }
+        for (int i = 0; i < n; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            long z = in.nextLong();
+            nodes[x].children.add(nodes[y]);
+            nodes[x].weight.add(z);
+        }
+
+        node[] heap = new node[n + 1];
+        int top = 1;
+
+
         out.close();
     }
+
+    public static void insert(node[] heap, int top, node num) {
+        heap[top] = num;
+        while (top > 1) {
+            if (heap[top].val < heap[top / 2].val) {
+                swap(heap, top, top / 2);
+                top = top / 2;
+            } else if (heap[top].val == heap[top / 2].val && heap[top].index < heap[top / 2].index) {
+                swap(heap, top, top / 2);
+                top = top / 2;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static node delete(node[] heap, int top) {
+//        System.out.println(heap[2].val+heap[3].val);
+        node re = heap[1];
+        heap[1] = heap[top - 1];
+        heap[top - 1] = null;
+        int t = 1;
+        while (t * 2 < top - 1) {
+            if (t * 2 + 1 == top - 1) {
+                if (heap[t * 2].val <= heap[t].val) {
+                    swap(heap, t, t * 2);
+                    t *= 2;
+                } else {
+                    return re;
+                }
+            } else {
+                if (heap[t].val <= heap[t * 2].val && heap[t].val <= heap[t * 2 + 1].val) {
+                    return re;
+                } else if (heap[2 * t].val <= heap[t].val && heap[2 * t].val <= heap[2 * t + 1].val) {
+                    swap(heap, t, 2 * t);
+                    t *= 2;
+                } else if (heap[2 * t + 1].val <= heap[t].val && heap[2 * t + 1].val <= heap[2 * t].val) {
+                    swap(heap, t, 2 * t + 1);
+                    t = 2 * t + 1;
+                }
+            }
+        }
+        return re;
+    }
+
+    public static void swap(node[] heap, int i, int j) {
+        node temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    public static class node {
+        int index;
+        long val;
+        ArrayList<node> children = new ArrayList<>();
+        ArrayList<Long> weight = new ArrayList<>();
+
+        public node() {
+        }
+
+        public node(int index, long val) {
+            this.index = index;
+            this.val = val;
+        }
+    }
+
     static class QReader {
         private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         private StringTokenizer tokenizer = new StringTokenizer("");
