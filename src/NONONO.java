136222c5f893aabf25f8a50445ea9a9ef42601da
aurora324 <3050517330@qq.com>
2024-01-13 21:12:59 +0800

    commit

--- src/NONONO.java
+++ src/NONONO.java
@@ -66,16 +66,14 @@ public class NONONO {
 
             if (!goon) {
                 for (int j = 1; j < xn; j++) {
-                    System.out.print(lNodes[j].value + " ");
+                    out.print(lNodes[j].value + " ");
                 }
-                System.out.print(lNodes[xn].value);
-                System.out.println();
-
+                out.println(lNodes[xn].value);
             }
 
 
             if (goon && point1.index == 1 && point2.index == xn) {
-                System.out.println();
+                out.println("");
                 goon = false;
             }
 
@@ -126,13 +124,13 @@ public class NONONO {
                     cur = lNodes[0];
                     while (cur.next.value != max) {
                         cur = cur.next;
-                        System.out.print(cur.value + " ");
+                        out.print(cur.value + " ");
                     }
-                    System.out.println();
+                    out.println("");
                 }
 
                 if (goon && point1.index == 1 && point2.index == xn) {
-                    System.out.println();
+                    out.println("");
                     goon = false;
                 }
             }
@@ -140,78 +138,105 @@ public class NONONO {
         out.close();
     }
 
-
-    static class QReader {
-        private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        private StringTokenizer tokenizer = new StringTokenizer("");
-
-        private String innerNextLine() {
-            try {
-                return reader.readLine();
-            } catch (IOException e) {
-                return null;
-            }
+    static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
         }
 
-        public boolean hasNext() {
-            while (!tokenizer.hasMoreTokens()) {
-                String nextLine = innerNextLine();
-                if (nextLine == null) {
-                    return false;
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg) return -ret;
+            else return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
                 }
-                tokenizer = new StringTokenizer(nextLine);
             }
-            return true;
+            if (neg)
+                return -ret;
+            return ret;
         }
 
-        public String nextLine() {
-            tokenizer = new StringTokenizer("");
-            return innerNextLine();
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
         }
 
-        public String next() {
-            hasNext();
-            return tokenizer.nextToken();
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
         }
 
-        public int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
         }
     }
-
-    static class QWriter implements Closeable {
-        private BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        public void print(Object object) {
-            try {
-                writer.write(object.toString());
-            } catch (IOException e) {
-                return;
-            }
-        }
-
-        public void println(Object object) {
-            try {
-                writer.write(object.toString());
-                writer.write("\n");
-            } catch (IOException e) {
-                return;
-            }
-        }
-
-        @Override
-        public void close() {
-            try {
-                writer.close();
-            } catch (IOException e) {
-                return;
-            }
-        }
-    }
-
-
 }
